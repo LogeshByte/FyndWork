b893f3c24fc19130980289457527f8e2843ffa66
Logeshwar <calogeshwar@gmail.com>
2025-06-30 11:24:10 +0530

    feat(auth): update authentication logic, add logout, and improve session redirect

---
+++ src/components/general/SubmitButtons.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+import { ReactNode } from "react";
+
+interface GeneralSubmitButtonsProps {
+    text:string;
+    variant?: "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | null | undefined;
+    width?: string;
+    icon?: ReactNode;
+}
+
+export function GeneralSubmitButtons({text,variant,width,icon}:GeneralSubmitButtonsProps){
+    const {pending} = useFormStatus();
+    return(
+        <Button variant={variant} className={width} disabled={pending}>
+             {pending ? (
+                <>
+                <Loader2 className="size-4 animate-spin"/>
+                <span>Submitting...</span>
+                </> ) :(
+                    <>
+                    {icon && <span className="mr-2">{icon}</span>}
+                    <span>{text}</span>
+                    </>
+                )
+             }
+        </Button>
+    )
+}
